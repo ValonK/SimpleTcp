5f05d1709a9aee993e4e288e0785353999e66e6d
valon <vk@looom.at>
2018-06-01 20:19:36 +0200

    Port completed

---
+++ SimpleTcp/SimpleTcp/Server/ConnectedClient.cs
@@ -0,0 +1,11 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace SimpleTcp.Server
+{
+    public class ConnectedClient
+    {
+        public IPAddress ServerIP { get; internal set; }
+        public TcpClient Client { get; internal set; }
+    }
+}
